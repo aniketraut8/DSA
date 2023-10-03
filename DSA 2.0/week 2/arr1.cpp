ebef922411f5ff25da047941bb03e4aa2d4a2b0c
ANIKET RAUT <aniketraut0926@gmail.com>
2023-10-03 22:50:02 +0530

    Dsa 2.0

---
+++ DSA 2.0/week 2/arr1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    //int arr[10];
+   // cout<<"Base add of Arr is:"<<&arr<<endl;
+   // cout<<"Size address of Arr is:"<<sizeof(arr)<<endl;
+    
+    // int arr[]={2,3,4,56,3,7,8,};
+    // int brr[5]={1,2,3,4,5};
+    // int crr[5]={2,3};  
+    // int drr[2]={1,2,3,4,5}; //error
+    
+    
+    
+    return 0;
+}
