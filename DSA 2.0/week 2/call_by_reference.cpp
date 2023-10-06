44a46107ff53cf2bb97afe1c1f252903c318bea9
ANIKET RAUT <aniketraut0926@gmail.com>
2023-10-07 05:07:43 +0530

    .

---
+++ DSA 2.0/week 2/call_by_reference.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+//call by value 
+int incrementBy1(int &n){  //this n is not copy of main n this is another memory block
+    n = n+1;
+    return n;
+} 
+
+int main(){
+    int n;
+    cin>>n;
+
+    incrementBy1(n);
+    cout<< "n:"<<n<<endl;
+}
