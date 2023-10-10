fe00f6e72ffa83eab2e17ee50ccdaec4c2882309
ANIKET RAUT <aniketraut0926@gmail.com>
2023-10-11 00:36:52 +0530

    .

---
+++ DSA 2.0/week 2/Vector_stl1.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void print(vector<int>v){
+    cout<<"Printing vector:"<<endl;
+    int size = v.size();
+    for(int i=0;i<size;i++){
+        cout<<v[i]<<endl;
+    }
+}
+
+int main(){
+ //in vector dont tell size of vectoe
+ //just keep inserting it will manage the allocation
+
+ vector<int>v; //aaray hi hai
+int n;
+cin>>n;
+
+for(int i=0;i<n;i++){
+    int d; cin>>d;
+    v.push_back(d);
+    cout<<"Capacity:"<<v.capacity()<<" Size: "<<v.size()<<endl;
+
+}
+ //insert
+//  v.push_back(1);
+//  v.push_back(2);
+//  v.push_back(3);
+//  v.push_back(4);
+//  v.push_back(5);
+
+ print(v);
+
+ cout<<"After inserting 70:"<<endl;;
+ v.push_back(70);
+ print(v);
+
+ v.clear();
+ print(v);
+}
