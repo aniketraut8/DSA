cc0d03dcb291358d64dce48591bf1f6fb10d3b00
ANIKET RAUT <aniketraut0926@gmail.com>
2023-10-17 23:29:19 +0530

    17/10/23

---
+++ DSA 2.0/week 2/Array_level3/Searching_in_2D.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<limits.h>
+using namespace std;
+
+bool findTarget(int arr[][3],int row,int col,int target){
+    for(int i=0;i<row;i++){
+        for(int j=0;j<col;j++){
+            if(arr[i][j]==target){
+                return true;
+            }
+          }
+    }
+    return false;
+}
+
+int findMax(int arr[][3],int row,int col){
+    int maxAns = INT_MIN;
+
+    for(int i=0;i<row;i++){
+        for(int j=0;j<col;j++){
+            if(arr[i][j]>maxAns){
+                maxAns = arr[i][j];
+            }
+        }
+    }
+    return maxAns;
+}
+
+
+
+int main(){
+    int arr[3][3]={
+        {12,3,2},
+        {23,34,11},
+        {1,8,5}
+    };
+    int row=3;
+    int col=3;
+    int target=34;
+
+    cout<<"Found or Not:"<<findTarget(arr,row,col,target)<<endl;
+
+   cout<<"Maximum Element is:"<<findMax(arr,row,col)<<endl;
+}
