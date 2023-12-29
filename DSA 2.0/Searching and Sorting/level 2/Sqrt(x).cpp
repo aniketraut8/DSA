8efc55a6a1bb01e1bc660ec17c618ed153e31563
ANIKET RAUT <aniketraut0926@gmail.com>
2023-12-29 23:25:36 +0530

    .

--- DSA 2.0/Searching and Sorting/level 2/Sqrt(x).cpp
+++ DSA 2.0/Searching and Sorting/level 2/Sqrt(x).cpp
@@ -2,31 +2,34 @@
 #include<vector>
 using namespace std;
 
-int mySqrt(int &x){
-    int s=0;
-    int e=x;
-    long long int mid=s+(e-s)/2;
-    int ans=-1;
+int mySqrt(int x) {
+    int s = 0;
+    int e = x;
+    int mid = 0; // Change the type from long long int to int
+    int ans = -1;
 
-    while(s<=e){
-        if(mid*mid == x){
+    while (s <= e) {
+        mid = s + (e - s) / 2;
+        long long int squr = static_cast<long long int>(mid) * mid; // Explicitly cast mid to long long int
+        if (squr == x) {
             return mid;
         }
-        else if(mid*mid<x){
-            ans=mid;
-            s=mid+1;
+        else if (squr < x) {
+            ans = mid;
+            s = mid + 1;
         }
-        else{
-            e=mid-1;
+        else {
+            e = mid - 1;
         }
-        mid=s+(e-s)/2;
     }
+
+    return ans; // Return ans if the exact square root is not found
 }
 
-int main(){
-    int n;
-    cin>>n;
-    
-    cout<<"Sqrt of x to nearby integer is:"<<mySqrt(n)<<endl;
+int main() {
+    int x = 10;
+  
+    int Ans = mySqrt(x);
+    cout << "Sqrt of x to nearby integer is: " << Ans << endl;
     return 0;
 }
---
+++ DSA 2.0/Searching and Sorting/level 3/Divide_BS.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+int getQuotient(int divisor,int dividend){
+    int s=0;
+    int e=dividend;
+    int ans;
+    int mid=s+(e-s)/2;
+
+    while(s<=e){
+        if(mid*divisor==dividend){
+            return mid;
+        }
+        if(mid*divisor<dividend){
+            ans=mid;
+            s=mid+1;
+        }
+        else{
+            e=mid-1;
+        }
+        mid=s+(e-s)/2;
+    }
+    return ans;
+}
+
+
+
+int main(){
+    int divisor=5;
+    int dividend=-35;
+
+    int ans=getQuotient(abs(divisor),abs(dividend));
+
+    if((divisor>0 && dividend<0)||(divisor<0 && dividend>0)){
+        ans=0-ans;
+    }
+    cout<<"Final Ans is:"<<ans<<endl;
+
+    return 0;
+}
