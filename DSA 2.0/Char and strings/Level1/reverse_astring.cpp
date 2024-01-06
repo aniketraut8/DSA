626696b21d6696f1677e8662dbb4ed8273b4b35e
ANIKET RAUT <aniketraut0926@gmail.com>
2024-01-06 23:32:29 +0530

    .

---
+++ DSA 2.0/Char and strings/Level1/reverse_astring.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+void reverseString(char ch[],int len){
+ int i=0;
+ int j=len-1;
+
+ while(i<=j){
+    
+        swap(ch[i],ch[j]);
+        i++;
+        j--;
+    
+ }
+
+}
+int main(){
+    char ch[100];
+   cin.getline(ch,100);
+    
+   cout<<"Before:"<<ch<<endl;
+   int len=strlen(ch);
+   reverseString(ch,len);
+   cout<<"After :"<<ch<<endl;
+
+}
