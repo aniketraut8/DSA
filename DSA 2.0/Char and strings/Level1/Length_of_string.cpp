86845369224bb8bdf8d104f147f18df0d3f7feda
ANIKET RAUT <aniketraut0926@gmail.com>
2024-01-06 23:20:31 +0530

    .

---
+++ DSA 2.0/Char and strings/Level1/Length_of_string.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+int findLength(char ch[],int size){
+    int length=0;
+    int index=0;
+    while(ch[index]!='\0'){
+        length++;
+        index++;
+    }
+    return length;
+}
+
+int main(){
+    char ch[100];
+    cin>>ch;
+
+    int len=findLength(ch,100);
+
+    cout<<"Length of String is:"<<len<<endl;
+    cout<<"Printing length:"<<strlen(ch)<<endl;
+}
---
+++ DSA 2.0/Searching and Sorting/Assignments_BS/k_diff_pairs_inarr.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<vector>
+#include<bits/stdc++.h>
+using namespace std;
+
+int findPairs(vector<int>&nums,int k){
+
+    sort(nums.begin(),nums.end());
+    set<pair<int,int>>ans;
+    int i=0,j=1;
+
+    while(j<nums.size()){
+        int diff=nums[j]-nums[i];
+        if(diff==k){
+            ans.insert({nums[i],nums[j]});
+            ++i,++j;        
+    }
+    else if(diff>k){
+        i++;
+    }
+    else{
+        j++;
+    }
+    if(i==j)j++;
+}
+return ans.size();
+}
+
+int main(){
+    vector<int>nums{3,1,4,1,5};
+    int k=2;
+
+ cout<<"Pairs are:"<<findPairs(nums,k);
+}
