3138b98b486dba8c5b52fe704f02481a4c0bfe67
ANIKET RAUT <aniketraut0926@gmail.com>
2024-01-08 22:48:32 +0530

    .

---
+++ DSA 2.0/Char and strings/Level1/check_palindrome.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+bool checkPalindrome(char ch[],int len){
+    int i=0;
+    int j=len-1;
+
+    while(i<=j){
+        if(ch[i]==ch[j]){
+            i++;
+            j--;
+        }
+        else{
+            return false;
+        }
+        //yaha pohoch gye to saare characters match hogaye
+        return true;
+    }
+}
+
+int main(){
+    char ch[100];
+    cin>>ch;
+    int len=strlen(ch);
+
+    bool isPalindrome=checkPalindrome(ch,len);
+
+    if(isPalindrome){
+        cout<<"Valid Palindrome"<<endl;
+    }
+    else cout<<"Invalid Palindrome"<<endl;
+}
