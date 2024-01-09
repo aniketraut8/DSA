15ebb5de98b16c278920e9fdb9add454d87baaff
ANIKET RAUT <aniketraut0926@gmail.com>
2024-01-09 22:48:44 +0530

    .

--- DSA 2.0/Char and strings/Level1/check_palindrome.cpp
+++ DSA 2.0/Char and strings/Level1/check_palindrome.cpp
@@ -14,7 +14,7 @@ bool checkPalindrome(char ch[],int len){
         else{
             return false;
         }
-        //yaha pohoch gye to saare characters match hogaye
+        // yaha pohoch gye to saare characters match hogaye
         return true;
     }
 }
